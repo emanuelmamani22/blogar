f0aa4685ebfc9e71021601bbf0df0ce613f37a72
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-01-12 11:24:56 -0300

    Formulario RegistroUser agregado

--- blogdb/forms.py
+++ blogdb/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Post
-
+from django.contrib.auth.models import User
 
 
 class EntradaForm(forms.Form):
@@ -13,3 +13,31 @@ class EntradaForm(forms.Form):
         if Post.objects.filter(titulo=titulo_blog):
           raise forms.ValidationError('Nombre repetido')
         return titulo_blog
+
+
+class RegistrarUser(forms.Form):
+     username=forms.CharField(min_length=5)
+     nombre=forms.CharField(min_length=5)
+     apellido=forms.CharField(min_length=5)
+     email=forms.EmailField()
+     password= forms.CharField(min_length=5, widget=forms.PasswordInput())
+     password2= forms.CharField(widget=forms.PasswordInput())
+     
+     def clean_username(self):
+        username=self.cleaned_data['username']
+        if User.objects.filter(username=username):
+           raise forms.ValidationError('El nombre de usario ya existe')
+        return username
+
+     def clean_email(self):
+        email=self.cleaned_data['email']
+        if User.objects.filter(email=email):
+           raise forms.ValidationError('Ese email ya esta asociado a una cuenta')
+        return email
+
+     def clean_password2(self):
+        password=self.cleaned_data['password']
+        password2=self.cleaned_data['password2']
+        if password != password2:
+           raise forms.ValidationError('El password no coincide')
+        return password2
