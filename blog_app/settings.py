22c0e2a1959bc8333d9ff605f19ca492bb4b4316
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-01-12 09:34:44 -0300

    Middleware agregado

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.media',
             ],
         },
     },
@@ -88,7 +89,7 @@ DATABASES = {
 	'USER': 'djangoemmanuel',
 	'PASSWORD': 'admindjango',
 	'HOST': 'localhost',
-	'PORT': '5432'
+	'PORT': '5432',
     }
 }
 
