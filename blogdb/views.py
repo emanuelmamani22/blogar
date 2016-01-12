d227cac655dce1d0a7e4a19926b057782beda53f
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-01-12 11:26:50 -0300

    Vista Registro agregada

--- blogdb/views.py
+++ blogdb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from .models import Post
-from .forms import EntradaForm
+from .forms import EntradaForm, RegistrarUser
 
 # Create your views here.
 
@@ -26,3 +26,24 @@ def entrada(request):
    else :
        form = EntradaForm()
    return render(request, 'entrada.html', {'form':form})
+
+
+def registrar(request):
+   if request.method == 'POST':
+     form=RegistrarUser(request.POST)
+     if form.is_valid():
+        user_f=request.POST['username']
+        name_f=request.POST['nombre']
+        namelast_f=request.POST['apellido']
+        email_f=request.POST['email']
+        password_f=request.POST['password']
+        
+        insert=User.objects.create_user(username=user_f, first_name=name_f, last_name=namelast_f, email=email_f, password=password_f)
+        insert.save()
+        
+        return HttpResponseRedirect('/')
+     else :
+        return render(request, 'registro.html', {'form':form})
+   else :
+        form=RegistrarUser()
+   return render(request, 'registro.html', {'form':form})
